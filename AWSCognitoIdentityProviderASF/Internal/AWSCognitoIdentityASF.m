aa8ac3dd8e0afb03691e734efa85d14e27720c12
Benjamin Bruckhart <ben@tasteseller.com>
2020-08-20 12:00:12 -0600

    Remove Cellular items from fingerprint

--- AWSCognitoIdentityProviderASF/Internal/AWSCognitoIdentityASF.m
+++ AWSCognitoIdentityProviderASF/Internal/AWSCognitoIdentityASF.m
@@ -60,11 +60,13 @@ static NSString *const AWSCognitoIdentityASFVersion= @"IOS20171114";
     NSString *timezoneOffset = [NSString stringWithFormat:@"%@:%@",hourOffset,minuteOffset];
     NSString * locale = [[NSLocale preferredLanguages] objectAtIndex:0];
     CTTelephonyNetworkInfo *networkInfo = [[CTTelephonyNetworkInfo alloc] init];
+    #if TARGET_OS_IOS && !TARGET_OS_MACCATALYST
     NSString * networkType = [networkInfo currentRadioAccessTechnology];
     CTCarrier *cellularProvider = [networkInfo subscriberCellularProvider];
     NSString *countryCode = cellularProvider.isoCountryCode;
     BOOL hasSimCard = countryCode != nil;
     NSString *carrier = [cellularProvider carrierName];
+    #endif
     NSString *fingerprint = [NSString stringWithFormat:@"Apple/%@/%@/-:%@/-/-:-/%@",
                                                         [AWSCognitoIdentityASF dashIfNil:[device model]],
                                                         [AWSCognitoIdentityASF dashIfNil:[AWSCognitoIdentityASF deviceName]],
@@ -91,9 +93,12 @@ static NSString *const AWSCognitoIdentityASFVersion= @"IOS20171114";
     [AWSCognitoIdentityASF addIfNotNil: contextData key:AWSCognitoIdentityDeviceHeight value: [NSString stringWithFormat:@"%.0f",screenHeight]];
     [AWSCognitoIdentityASF addIfNotNil: contextData key:AWSCognitoIdentityDeviceWidth value: [NSString stringWithFormat:@"%.0f",screenWidth]];
     [AWSCognitoIdentityASF addIfNotNil: contextData key:AWSCognitoIdentityDeviceLanguage value: locale];
+    
+    #if TARGET_OS_IOS && !TARGET_OS_MACCATALYST
     [AWSCognitoIdentityASF addIfNotNil: contextData key:AWSCognitoIdentityCarrier value: carrier];
     [AWSCognitoIdentityASF addIfNotNil: contextData key:AWSCognitoIdentityNetworkType value: networkType];
     [AWSCognitoIdentityASF addIfNotNil: contextData key:AWSCognitoIdentityHasSimCard value:hasSimCard?@"true":@"false"];
+    #endif
     
     [AWSCognitoIdentityASF addIfNotNil: contextData key:AWSCognitoIdentityDeviceFingerprint value:fingerprint];
     
